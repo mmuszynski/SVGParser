432059656de646be49ac7422a9b8fd3b4c93a3cd
Mike <mmuszynski@gmail.com>
2019-12-01 13:14:48 -0500

    Initial Commit

---
+++ SVGParser/SVGParser.h
@@ -0,0 +1,19 @@
+//
+//  SVGParser.h
+//  SVGParser
+//
+//  Created by Mike Muszynski on 12/1/19.
+//  Copyright © 2019 Mike Muszynski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SVGParser.
+FOUNDATION_EXPORT double SVGParserVersionNumber;
+
+//! Project version string for SVGParser.
+FOUNDATION_EXPORT const unsigned char SVGParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVGParser/PublicHeader.h>
+
+
---
+++ SVGParserTests/SVGParserTests.swift
@@ -0,0 +1,34 @@
+//
+//  SVGParserTests.swift
+//  SVGParserTests
+//
+//  Created by Mike Muszynski on 12/1/19.
+//  Copyright © 2019 Mike Muszynski. All rights reserved.
+//
+
+import XCTest
+@testable import SVGParser
+
+class SVGParserTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
