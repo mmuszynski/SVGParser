adcf25b71e8849abdb78057d212023ab0cd5373b
Mike Muszynski <mmuszynski@gmail.com>
2020-08-25 12:12:11 -0400

    getting ready to transition into swift package

---
+++ SVGParser_macOS/SVGParser_macOS.h
@@ -0,0 +1,19 @@
+//
+//  SVGParser_macOS.h
+//  SVGParser_macOS
+//
+//  Created by Mike Muszynski on 12/7/19.
+//  Copyright © 2019 Mike Muszynski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SVGParser_macOS.
+FOUNDATION_EXPORT double SVGParser_macOSVersionNumber;
+
+//! Project version string for SVGParser_macOS.
+FOUNDATION_EXPORT const unsigned char SVGParser_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVGParser_macOS/PublicHeader.h>
+
+
